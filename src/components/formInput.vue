2d33b92a2ad056508ea5f990221a0f4d6343a3ef
Ron <ronchang717@gmail.com>
2023-11-20 15:03:03 +0800

    feat: Fix form input styles and add validation for number inputs

--- src/components/formInput.vue
+++ src/components/formInput.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="form_item formItem">
-    <p>
+    <p v-if="props.title?.length">
       {{ props.title }}:
       <br />
       <span v-if="props.subTitle?.length">({{ props.subTitle }})</span>
     </p>
-    <template v-if="props.type === 'number'">
+    <template v-if="props.type === 'number-outline'">
       <input
         class="form-md outline_w"
         type="number"
-        :placeholder="placeholder"
         v-model="val"
+        :placeholder="placeholder"
+        :min="props.min"
+        :max="props.max"
         @blur="$emit('setValue', val)"
       />
-      <label :data-domain="props.unit"></label>
+      <label class="outlineLabel" :data-domain="props.unit"></label>
+    </template>
+    <template v-if="props.type === 'number'">
+      <input
+        class="form-md number_blue"
+        type="number"
+        v-model="val"
+        :placeholder="placeholder"
+        :step="props.step"
+        :min="props.min"
+        :max="props.max"
+        @blur="$emit('setValue', val)"
+      />
+      <label class="numberLabel" :data-domain="props.unit"></label>
     </template>
     <template v-if="props.type === 'select'">
       <div class="d-flex selectItem">
@@ -32,7 +47,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 const props = defineProps({
   type: String,
   title: String,
@@ -40,12 +55,36 @@ const props = defineProps({
   unit: String,
   btnList: Array,
   btnVal: String,
+  min: {
+    type: Number,
+    default: 0,
+  },
+  max: {
+    type: Number,
+    default: 0,
+  },
   placeholder: {
     type: Number,
     default: 0,
   },
+  step: {
+    type: Number,
+    default: 1,
+  },
 })
 const val = ref(null)
+
+watch(
+  () => val.value,
+  (newVal) => {
+    console.log('val:', newVal)
+    if (newVal > props.max) {
+      val.value = props.max
+    } else if (newVal < props.min) {
+      val.value = props.min
+    }
+  }
+)
 </script>
 
 <style lang="scss" scoped></style>
